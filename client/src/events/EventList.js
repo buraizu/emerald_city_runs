98bba3d54d9bba4f1e42e103726afef2fcf23438
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-24 14:03:26 -0700

    Import Router components

--- client/src/events/EventList.js
+++ client/src/events/EventList.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const EventList = (props) => {
   console.log(props.runEvents);
-  let displayEvents;
-  if (props.runEvents.events.length > 0) {
-    displayEvents = props.runEvents.events.map((run, index) => <li key={index}>{run.title} -- <a href={run.url} target="_blank">Check it out</a></li>)
-  }
+
+  const displayEvents = props.runEvents.events.map((run, index) => <li key={index}>{run.date} -- {run.title} -- <a href={run.url} target="_blank">Check it out</a></li>)
+
   return (
     <ul>
       {displayEvents}
