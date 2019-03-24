908f6689bdf456eef11794d80d711a8a500d959c
github username <github email address>
2019-03-24 14:29:50 -0700

    Formatting changes for EventList

--- client/src/events/EventList.js
+++ client/src/events/EventList.js
@@ -3,7 +3,17 @@ import React from 'react';
 const EventList = (props) => {
   console.log(props.runEvents);
 
-  const displayEvents = props.runEvents.events.map((run, index) => <li key={index}>{run.date} -- {run.title} -- <a href={run.url} target="_blank">Check it out</a></li>)
+  const displayEvents = props.runEvents.events.map((run, index) =>
+    <li key={index}>
+      {run.date} --
+      {run.title} -- 
+      <a href={run.url}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Check it out
+      </a>
+    </li>)
 
   return (
     <ul>
