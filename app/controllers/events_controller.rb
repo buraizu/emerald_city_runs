2a61f5bc3efe3086a43dba53f44e7a6137ee2df0
github username <github email address>
2019-03-23 19:33:17 -0700

    Create initializer file to pull Active API Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,13 @@
 class EventsController < ApplicationController
 
   def index
-    api_key = ENV['ACTIVE_API_KEY']
-    @events = RestClient::Request.execute(
-      method: :get,
-      url: "http://api.amp.active.com/v2/search?query=running&category=races&city=Seattle&radius=50&start_date=2019-03-22..&api_key=#{api_key}"
-    )
-    render json: @events, status: 200
+
+    events = Event.all
+    # RestClient::Request.execute(
+    #   method: :get,
+    #   url: "http://api.amp.active.com/v2/search?query=running&category=races&city=Seattle&radius=50&start_date=2019-03-22..&api_key=#{api_key}"
+    # )
+    render json: events, status: 200
   end
 
 end
---
+++ config/initializers/rest_client.rb
@@ -0,0 +1,24 @@
+require 'rest-client'
+require 'rails/configuration'
+
+  active_url = "http://api.amp.active.com/v2/search?query=running&category=races&city=Seattle&radius=50&start_date=2019-03-22..&api_key="
+  api_key = ENV['ACTIVE_API_KEY']
+
+  data = JSON.parse( RestClient.get("#{active_url}#{api_key}"))
+
+  data["results"].each do |result|
+
+    existing_event = Event.find_by(title: result["assetName"]) # Check if event is already in db
+
+    if !existing_event
+      newEvent = Event.new
+      newEvent.title = result["assetName"]
+      newEvent.url = result["homePageUrlAdr"]
+      newEvent.date = result["activityStartDate"]
+      if newEvent.save
+        puts "saved event from Active api"
+      else
+        puts "did not save event"
+      end
+    end
+  end
