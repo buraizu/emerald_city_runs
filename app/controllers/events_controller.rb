e65e1acc7cf9d4ed4abca13a6e0fe941a8066eb6
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-24 14:29:50 -0700

    Formatting changes for EventList

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,9 +1,10 @@
 class EventsController < ApplicationController
 
   def index
-
+    current_date = Time.now.strftime("%Y-%m-%d")
+    # filteredEvents = Event.all.map {|e| e.date >= current_date}
     events = Event.all
-  
+
     render json: events, status: 200
   end
 
