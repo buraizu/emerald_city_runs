46d476459aaf5126ca051fc7a022bac7023e970e
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-25 02:29:46 -0700

    Add Router Links to Header component

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => (
   <header>
     <ul>
-      <li></li>
-      <li></li>
+      <li><Link to="/events">Events</Link></li>
+      <li><Link to="/runs">Runs</Link></li>
     </ul>
   </header>
 )
