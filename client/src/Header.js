ae51cd046446177620b9eff177d2a30347927fe0
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-25 13:40:17 -0700

    Create Home.js and add NavLink to Header

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Header = () => (
   <header>
     <ul>
+      <li><NavLink exact to="/">Home</NavLink></li>
       <li><NavLink to="/events">Events</NavLink></li>
       <li><NavLink to="/runs">Runs</NavLink></li>
     </ul>
