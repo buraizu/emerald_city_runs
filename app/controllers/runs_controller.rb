fe5cbb6c635bc0f7b82d646b9236088fc71d35ef
github username <github email address>
2019-03-25 22:19:06 -0700

    Add Run resource to API Scope in routes.rb. Build out Runs controller

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,2 +1,20 @@
 class RunsController < ApplicationController
+
+  def create
+    @run = Run.new(run_params)
+
+    if @run.save
+      render component: 'Home'
+    else
+      render text: "Something went wrong"
+    end
+
+  end
+
+  private
+
+  def run_params
+    params.require(:run).permit(:course, :distance, :time, :review, :rating)
+  end
+
 end
