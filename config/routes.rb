ae3383f576b669d5f18997174e7e3cdadbc815d0
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-25 22:19:06 -0700

    Add Run resource to API Scope in routes.rb. Build out Runs controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :runs
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   scope '/api' do
     resources :events
-
+    resources :runs
   end
 end
