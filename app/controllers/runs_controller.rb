3d53b3c148f2e118c908eb74c14e7e64ba9fb30b
github username <github email address>
2019-03-28 21:52:17 -0700

    Save runs to Rails db

---
+++ app/controllers/runs_controller.rb
@@ -0,0 +1,24 @@
+class RunsController < ApplicationController
+
+  def index
+    runs = Run.all
+    render json: runs, status: 200
+  end
+
+  def create
+
+    @run = Run.new(run_params)
+
+    if @run.save
+      render json: @run
+    end
+
+  end
+
+  private
+
+  def run_params
+    params.require(:run).permit(:course, :distance, :time, :review, :rating)
+  end
+
+end
