d505746f98c52c2aa9ef40393ccff2902157bca2
github username <github email address>
2019-03-30 02:38:19 -0700

    Debug Run display issue

--- client/src/actions/runActions.js
+++ client/src/actions/runActions.js
@@ -1,45 +1,5 @@
-export const postRun = (run) => {
-
-  let data = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(run)
-  }
-
-  return fetch('api/runs', data)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    .catch(error => console.log(error))
-
-}
-
-export function postRun2(run) {
-
-  let data = {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(run)
-  }
-
-  let savedRun = fetch('api/runs', data)
-    .then(response => response.json())
-    .then(responseJson => console.log(responseJson))
-    .catch(error => console.log(error))
-
-  return (dispatch) => {
-    dispatch({
-      type:
-    })
-  }
-}
-
-export function postRun3() {
+// need to connect this file
+export function postRun() {
 
   let data = {
     method: 'POST',
