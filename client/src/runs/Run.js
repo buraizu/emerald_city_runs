4f4db16257845426aefae9f457338f77dcd4607e
Bryce Eadie <bryce.eadie@gmail.com>
2019-03-30 15:40:35 -0700

    Format Run component to display Run data

--- client/src/runs/Run.js
+++ client/src/runs/Run.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const Run = () => {
-
+const Run = (props) => {
+  console.log(props)
   return (
-    <div>This is a Run</div>
+    <div>
+      <h3>Course: {props.run.course}</h3>
+      <p>Distance: {props.run.distance}</p>
+      <p>Time: {props.run.time}</p>
+      <p>Review: {props.run.review}</p>
+      <p>Rating: {props.run.rating}</p>
+      <p>------------</p>
+    </div>
   )
 
 }
 
 export default Run;
+
+// <li key={index}>
+//   Course: {run.course}
+//   <br />
+//   Distance: {run.distance}
+//   <br />
+//   Time: {run.time}
+//   <br />
+//   Review: {run.review}
+//   <br />
+//   Rating: {run.rating}
+//   <br />
+//   <button onClick={props.handleDelete}>Delete</button>
+// </li>
--- client/src/runs/RunsList.js
+++ client/src/runs/RunsList.js
@@ -5,20 +5,7 @@ import Run from './Run';
 const RunsList = (props) => {
 
   const displayRuns = props.runs.runs.map((run, index) =>
-    <Run id={index} key={index} run={run} />
-    // <li key={index}>
-    //   Course: {run.course}
-    //   <br />
-    //   Distance: {run.distance}
-    //   <br />
-    //   Time: {run.time}
-    //   <br />
-    //   Review: {run.review}
-    //   <br />
-    //   Rating: {run.rating}
-    //   <br />
-    //   <button onClick={props.handleDelete}>Delete</button>
-    // </li>
+    <Run id={run.id} key={run.id} run={run} />
   )
 
   return (
