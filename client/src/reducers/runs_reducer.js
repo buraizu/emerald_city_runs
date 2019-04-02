f4eb69ea706fbcd2dd1757511bf99c49f52c2cab
github username <github email address>
2019-04-02 01:26:42 -0700

    Add 'featured' column to Event table. Format runs_reducer

--- client/src/reducers/runs_reducer.js
+++ client/src/reducers/runs_reducer.js
@@ -3,23 +3,18 @@ import { combineReducers } from 'redux';
 export default function runsReducer(state = { loading: false, runs: [] }, action) {
 
   switch(action.type) {
-
     case 'LOADING_RUNS':
-
       return Object.assign({}, state, {loading: true});
 
     case 'FETCH_RUNS':
-
       return {loading: false, runs: action.payload};
 
     case 'ADD_RUN':
-
       return {
         runs: [...state.runs, action.run]
       };
 
     case 'UPDATE_RUN':
-
       return {
         runs: [
           ...state.runs.filter(run => run.id !== action.run.id),
@@ -28,13 +23,10 @@ export default function runsReducer(state = { loading: false, runs: [] }, action
       }
 
     case 'DELETE_RUN':
-
       const idOfRunToDelete = action.run.id
-
       const newState = Object.assign([], state.runs.filter(run => {
         return run.id != idOfRunToDelete
       }));
-
       return newState;
 
     default:
