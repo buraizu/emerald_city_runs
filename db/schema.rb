c7ad88381fb39af910f57943ac5f464b0b27f581
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-02 01:26:42 -0700

    Add 'featured' column to Event table. Format runs_reducer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_26_035652) do
+ActiveRecord::Schema.define(version: 2019_04_02_082408) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_03_26_035652) do
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "featured", default: false
   end
 
   create_table "runs", force: :cascade do |t|
