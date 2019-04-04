44f65e601d38e7d0799ae91737c3081da0b3904b
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-04 02:47:02 -0700

    Move connect function to EventsContainer from App.js

--- client/src/events/EventsContainer.js
+++ client/src/events/EventsContainer.js
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import * as actions from '../actions/eventActions.js';   // need new action file
+import { bindActionCreators } from 'redux';
 
 import EventList from './EventList';
+import FeaturedEvent from './FeaturedEvent';
 
-const EventsContainer = (props) => {
 
-  return (
-    <div>
-      <h2>Events Container</h2>
-      <EventList runEvents={props.events} setEvent={props.setEvent} />
-    </div>
-  )
+
+// var found = array1.find(function(element) {
+//   return element > 10;
+// });
+class EventsContainer extends Component {
+
+  componentWillMount() {
+    this.props.fetchEvents();
+  }
+
+  getFeaturedEvent = () => {
+    let featuredEvent = this.props.events.events.find((event) => event.featured === true)
+    if(this.props.events.events.length > 0 && featuredEvent) {
+      return (
+        <div>
+          <FeaturedEvent event={featuredEvent} />
+        </div>
+      )
+    } else {
+      return null;
+    }
+  }
+
+  componentDidMount() {
+    return this.getFeaturedEvent();
+  }
+
+
+  render() {
+    let featuredEvent = this.getFeaturedEvent()
+
+    if(!featuredEvent) {
+      return (
+        <div>
+          <h2>Events Container</h2>
+          <EventList runEvents={this.props.events} setEvent={this.props.setEvent} />
+        </div>
+      )
+    } else {
+      return <FeaturedEvent event={featuredEvent} />
+    }
+
+  }
+
+
 
 }
 
-export default EventsContainer;
+const mapStateToProps = (state) => {
+  return {
+    events: state.events
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, {...actions})(EventsContainer);
