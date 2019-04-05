36434ecdb9aa0f593df4ce8784464fc70f8cbb24
github username <github email address>
2019-04-04 22:20:16 -0700

    Toggle 'featured' status of Events

--- client/src/events/EventsContainer.js
+++ client/src/events/EventsContainer.js
@@ -8,49 +8,20 @@ import EventList from './EventList';
 import FeaturedEvent from './FeaturedEvent';
 
 
-
-// var found = array1.find(function(element) {
-//   return element > 10;
-// });
 class EventsContainer extends Component {
 
-  componentWillMount() {
+  componentDidMount() {
     this.props.fetchEvents();
   }
 
-  getFeaturedEvent = () => {
-    let featuredEvent = this.props.events.events.find((event) => event.featured === true)
-    if(this.props.events.events.length > 0 && featuredEvent) {
-      return (
-        <div>
-          <FeaturedEvent event={featuredEvent} />
-        </div>
-      )
-    } else {
-      return null;
-    }
-  }
-
-  componentDidMount() {
-    return this.getFeaturedEvent();
-  }
-
-
   render() {
-    let featuredEvent = this.getFeaturedEvent()
-
-    if(!featuredEvent) {
-      return (
+    return (
         <div>
           <h2>Events Container</h2>
           <EventList runEvents={this.props.events} setEvent={this.props.setEvent} />
         </div>
       )
-    } else {
-      return <FeaturedEvent event={featuredEvent} />
     }
-
-  }
 
 
 
@@ -58,7 +29,8 @@ class EventsContainer extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    events: state.events
+    events: state.events,
+    featuredEvent: state.featuredEvent
   }
 }
 
