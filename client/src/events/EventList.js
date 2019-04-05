07377df3774d673949f3a623c17ca7d8938b2158
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-04 22:20:16 -0700

    Toggle 'featured' status of Events

--- client/src/events/EventList.js
+++ client/src/events/EventList.js
@@ -1,19 +1,25 @@
 import React from 'react';
 
 import EventPage from './EventPage';
+import FeaturedEvent from './FeaturedEvent';
 
 const EventList = (props) => {
 
-  console.log(props.runEvents);
+  console.log(props.runEvents.featuredEvent);
 
   const displayEvents = props.runEvents.events.map((event, index) => {
     return <EventPage key={index} runEvent={event} />
   })
 
   return (
-    <ul>
-      {displayEvents}
-    </ul>
+    <div>
+      <div>
+        <FeaturedEvent feature={props.runEvents.featuredEvent} />
+      </div>
+      <ul>
+        {displayEvents}
+      </ul>
+    </div>
   );
 }
 
--- client/src/events/FeaturedEvent.js
+++ client/src/events/FeaturedEvent.js
@@ -3,9 +3,12 @@ import React from 'react';
 const FeaturedEvent = (props) => {
 
   return (
+    
     <div>
-      <h3>You're currently training for {props.event.props.children.props.event.title}!</h3>
-      <a href={props.event.props.children.props.event.url} target="_blank" rel="noopener noreferrer">Event Home Page</a>
+      <h3>You're currently training for {props.feature.title}</h3>
+      <a href={props.feature.url} target="_blank" rel="noopener noreferrer">Event Home Page</a>
+      <p>Date: {props.feature.date}</p>
+      <p>id: {props.feature.id}</p>
     </div>
   )
 }
