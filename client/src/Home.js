5c746f7709b17ecd253646652e595accaf1c1dcf
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-06 15:47:03 -0700

    Add connect, FeaturedEvent, and LatestRun to Home component

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,9 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from './actions/eventActions.js';   // need new action file
+import { bindActionCreators } from 'redux';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
 
-const Home = () => {
-  return (
-    <div>This is the Home Page</div>
-  )
+import EventsContainer from './events/EventsContainer';
+import FeaturedEvent from './events/FeaturedEvent';
+import RunsContainer from './runs/RunsContainer';
+import LatestRun from './runs/LatestRun';
+
+class Home extends Component {
+
+  constructor(props, context) {
+    super(props, context);
+
+    };
+
+  componentDidMount() {
+    this.props.fetchEvents();
+    this.props.fetchRuns();
+  }
+
+  render() {
+
+    return (
+      <div>
+        <Container>
+          <Row>
+
+          </Row>
+          <Row>
+            <Col md={4}>
+              <FeaturedEvent feature={this.props.events.featuredEvent} />
+            </Col>
+            <Col md={{ span: 4, offset: 4 }}>
+              <LatestRun runs={this.props.runs} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    runs: state.runs,
+    events: state.events
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return { actions: bindActionCreators(actions, dispatch) }
+}
+
+export default connect(mapStateToProps, {...actions})(Home);
