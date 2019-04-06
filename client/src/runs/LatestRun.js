f2f4185a84b7d52d7664d7e2135200d9e926503b
github username <github email address>
2019-04-06 16:00:49 -0700

    Add PropTypes

--- client/src/runs/LatestRun.js
+++ client/src/runs/LatestRun.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
 
 const LatestRun = (props) => {
   let run = props.runs.runs[0];
@@ -25,6 +26,10 @@ const LatestRun = (props) => {
     )
   }
 
+}
+
+LatestRun.propTypes = {
+  run: PropTypes.object
 }
 
 export default LatestRun;
