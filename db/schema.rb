33d553f26326f4a5bfd1256af9c86a7b21fac8fb
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-15 14:50:16 -0700

    Add Users table to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_08_224602) do
+ActiveRecord::Schema.define(version: 2019_04_12_225722) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 2019_04_08_224602) do
     t.integer "time"
     t.text "review"
     t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
