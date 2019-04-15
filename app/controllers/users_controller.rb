d555344c595813464587427015cdc9663faf158e
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-15 14:48:11 -0700

    Set up controllers for User auth

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid? && @user.save
+      render json: @user
+    else
+      render json: @user.errors, status: 400
+    end
+  end
+
+  def find
+    @user = User.find_by(email: params[:user][:email])
+    if @user
+      render json: @user
+    else
+      @errors = @user.errors.full_messages
+      render json: @errors
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
