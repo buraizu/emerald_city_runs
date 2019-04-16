fbbfa0a373a06cc3598c53e199717581e1056c43
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-15 20:53:42 -0700

    Remove unused code

--- client/src/SignUp.js
+++ client/src/SignUp.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as actions from './actions/index';
 
@@ -58,12 +57,6 @@ class SignUp extends Component {
     )
   }
 
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(actions, dispatch)
-  }
 }
 
 export default connect(null, {...actions})(SignUp);
