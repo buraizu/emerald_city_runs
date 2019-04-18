466126bfbc2ba1a333577acdbb52bf93324347d9
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-18 01:06:40 -0700

    Implement database and controller associations for users and runs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_12_225722) do
+ActiveRecord::Schema.define(version: 2019_04_18_054143) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2019_04_12_225722) do
     t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_runs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
