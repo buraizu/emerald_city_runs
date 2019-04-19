da182a32211fd6997345aa5440fbda4019350dd5
github username <github email address>
2019-04-18 21:37:34 -0700

    Build out auth restrictions for Run resource

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,17 +1,19 @@
 class RunsController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user, only: [:update, :destroy, :create]
 
   def index
-    runs = Run.all
-    render json: runs, status: 200
+    @runs = Run.all
+    render json: @runs, status: 200
   end
 
   def create
     @run = Run.new(run_params)
+    @run.user_id = 1
     if @run.save
       render json: @run
     end
   end
+
 
   def update
     @run = Run.find_by(id: params[:id])
