1abaf043fc175dbebe703b068dcae5682ff4682c
Bryce Eadie <bryce.eadie@gmail.com>
2019-04-21 21:21:32 -0700

    Adjust formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,8 @@ class UsersController < ApplicationController
 
   private
 
-  def user_params
-    params.require(:user).permit(:email, :password)
-  end
+    def user_params
+      params.require(:user).permit(:email, :password)
+    end
 
 end
