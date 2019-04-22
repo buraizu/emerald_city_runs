2b3b0bff9c6172714740f2f2c7eb8a8c9ddac04c
github username <github email address>
2019-04-22 16:31:38 -0700

    Add cases for loading and fetching

--- client/src/reducers/userEventsReducer.js
+++ client/src/reducers/userEventsReducer.js
@@ -3,10 +3,16 @@ export default (state = { loading: false, userEvents: [] }, action) => {
   switch(action.type) {
 
     case 'ADD_USER_EVENT':
-      
       return {
         userEvents: [action.userEvent, ...state.userEvents]
       };
+
+    case 'LOADING_USER_EVENTS':
+      return Object.assign({}, state, {loading: true});
+
+    case 'FETCH_USER_EVENTS':
+      let orderedUserEvents = action.payload.reverse()
+      return {loading: false, userEvents: orderedUserEvents};
 
     default:
       return state;
