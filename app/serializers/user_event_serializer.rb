dbf043567c74292d24e6786d14224be0db96abf8
github username <github email address>
2019-04-21 21:20:54 -0700

    Add relational associations

--- app/serializers/user_event_serializer.rb
+++ app/serializers/user_event_serializer.rb
@@ -1,5 +1,5 @@
 class UserEventSerializer < ActiveModel::Serializer
-  attributes :id, :goal, :result
+  attributes :id, :goal, :result, :title, :url, :date
   belongs_to :user
   belongs_to :event
 end
