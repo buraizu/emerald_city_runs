6ba374fdaf92b96e490394baa937d3ebeb2ebe89
github username <github email address>
2019-04-23 01:54:26 -0700

    Build out display of past UserEvents

--- client/src/user_events/UserEventsList.js
+++ client/src/user_events/UserEventsList.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import UserEventDetail from './UserEventDetail';
+import PastUserEventForm from './PastUserEventForm';
 
 const UserEventsList = (props) => {
+  let currentDate = new Date();
   let displayUserEvents = <div>Nothing to display at the moment. Try refreshing.</div>
+  let pastUserEvents = <div>Check back here after your event to record your result!</div>
 
   if(props.userEvents.userEvents !== undefined) {
-    displayUserEvents = props.userEvents.userEvents.map((userEvent, index) =>
+    displayUserEvents = props.userEvents.userEvents.map((userEvent) =>
         <UserEventDetail key={userEvent.id} userEvent={userEvent} />
       )
+    // pastUserEvents = props.userEvents.userEvents.filter((userEvent) => userEvent.date < currentDate)
+    // let displayPastUserEvents = pastUserEvents.map((userEvent) =>
+    //     <PastUserEventForm key={userEvent.id} userEvent={userEvent} />
+    //   )
+
   }
 
   return (
@@ -18,6 +26,10 @@ const UserEventsList = (props) => {
       <br />
       <div>
         {displayUserEvents}
+      </div>
+      <div className="feature">
+        <h3>Your past events</h3>
+        {pastUserEvents}
       </div>
     </div>
   )
