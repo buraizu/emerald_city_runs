fdd6f8994444e3c6dd393600f6e3e0c45e6ee4a9
github username <github email address>
2019-04-22 22:53:14 -0700

    Build out goal setting functionality

--- client/src/actions/userEventActions.js
+++ client/src/actions/userEventActions.js
@@ -37,6 +37,27 @@ export const setEvent = (event) => {
   }
 }
 
+export const setGoal = (userEvent) => {
+  let userEventId = userEvent.id;
+  let data = {
+    method: 'PUT',
+    headers: {
+      "Authorization": `Bearer ${localStorage.token}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(userEvent)
+  }
+
+  return function(dispatch) {
+    return fetch(`/api/user_events/${userEventId}`, data)
+      .then(response => response.json())
+      .then(responseJson => {
+        dispatch({type: 'UPDATE_USER_EVENT', userEvent: responseJson})
+      })
+  }
+}
+
 // export const setEvent = (feature) => {
 //   feature.updated_at = new Date();
 //   let featureId = feature.id;
