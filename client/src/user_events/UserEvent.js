7b2518a1f529266c9ceb1a95257b4ce50315e2b0
github username <github email address>
2019-04-25 02:36:34 -0700

    Create UserEvent component

---
+++ client/src/user_events/UserEvent.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+const UserEvent = ({userEvent}) => {
+  let eventDate = new Date(userEvent.date);
+  let currentDate = new Date();
+  let buttonText = (currentDate < eventDate ? "Set your goal" : "Record your result")
+  return (
+    <div className="feature">
+      <h3>Your Event: {userEvent.title}</h3>
+      <p>Date: {userEvent.date}</p>
+      <p><a href={userEvent.url} target="_blank" rel="noopener noreferrer">Event Home Page</a></p>
+      <Link to={'/user_events/' + userEvent.id}>{buttonText}</Link>
+      <p>------------</p>
+    </div>
+  )
+
+}
+
+UserEvent.propTypes = {
+
+}
+
+export default UserEvent;
