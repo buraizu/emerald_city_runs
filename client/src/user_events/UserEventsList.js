af840fc770cafc73cb1e3bdbb3e99e267e2141bd
github username <github email address>
2019-04-25 02:38:20 -0700

    Implement distinction between upcoming and completed events

--- client/src/user_events/UserEventsList.js
+++ client/src/user_events/UserEventsList.js
@@ -2,21 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import UserEventDetail from './UserEventDetail';
 import PastUserEventForm from './PastUserEventForm';
+import UserEvent from './UserEvent';
 
 const UserEventsList = (props) => {
   let currentDate = new Date();
-  let displayUserEvents = <div>Nothing to display at the moment. Try refreshing.</div>
-  let pastUserEvents = <div>Check back here after your event to record your result!</div>
+
+  let displayUpcomingUserEvents = <div>Nothing to display at the moment. Try refreshing.</div>
+  let displayPastUserEvents = <div>Check back here after your event to record your result!</div>
 
   if(props.userEvents.userEvents !== undefined) {
-    displayUserEvents = props.userEvents.userEvents.map((userEvent) =>
-        <UserEventDetail key={userEvent.id} userEvent={userEvent} />
-      )
-    // pastUserEvents = props.userEvents.userEvents.filter((userEvent) => userEvent.date < currentDate)
-    // let displayPastUserEvents = pastUserEvents.map((userEvent) =>
-    //     <PastUserEventForm key={userEvent.id} userEvent={userEvent} />
-    //   )
+    let pastUserEvents = props.userEvents.userEvents.filter((userEvent) => new Date(userEvent.date) < currentDate)
+    let upcomingUserEvents = props.userEvents.userEvents.filter((userEvent) => new Date(userEvent.date) > currentDate)
 
+    displayUpcomingUserEvents = upcomingUserEvents.map((userEvent) =>
+        <UserEvent key={userEvent.id} userEvent={userEvent} />
+      )
+    displayPastUserEvents = pastUserEvents.map((userEvent) =>
+        <UserEvent key={userEvent.id} userEvent={userEvent} />
+      )
+
+    // let sortedEvents = action.payload.sort((event1, event2) => new Date(event1.date) - new Date(event2.date))
   }
 
   return (
@@ -24,12 +29,13 @@ const UserEventsList = (props) => {
       <br />
       <h3 className="displayText">Your Events</h3>
       <br />
-      <div>
-        {displayUserEvents}
+      <div className="feature">
+        <h3>Your upcoming events</h3>
+        {displayUpcomingUserEvents}
       </div>
       <div className="feature">
         <h3>Your past events</h3>
-        {pastUserEvents}
+        {displayPastUserEvents}
       </div>
     </div>
   )
